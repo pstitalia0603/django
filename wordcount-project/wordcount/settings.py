ff77193c4c29a3af045efa3095ff9d84306a82f7
PSF <48135781+pstitalia0603@users.noreply.github.com>
2022-06-11 16:03:14 -0400

    Update settings.py

--- wordcount-project/wordcount/settings.py
+++ wordcount-project/wordcount/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3%o5jp8ws$&^c54kk=x-6g1fox%nr5enl%%obg7p6ej$$1hv4#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
